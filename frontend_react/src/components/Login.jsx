0be3b3d3431090559fce5baa4122565c6987889d
Dan <daniel.liemng@gmail.com>
2022-05-19 23:57:06 -0400

    initial setup

---
+++ frontend_react/src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logo.png';
+
+const Login = () => {
+  return (
+    <div className='flex flex-col justify-center items-center h-screen'>
+      Login
+    </div>
+  );
+};
+
+export default Login;
